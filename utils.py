7eaff89961500db9f1e72de0dcd6542e12677ebc
ZCBhope <74125130+ZCBhope@users.noreply.github.com>
2020-11-20 22:52:09 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,47 @@
+import torch
+'''
+Those code from order-aware network.c
+'''
+def batch_episym(x1, x2, F):
+    batch_size, num_pts = x1.shape[0], x1.shape[1]
+    x1 = torch.cat([x1, x1.new_ones(batch_size, num_pts,1)], dim=-1).reshape(batch_size, num_pts,3,1)
+    x2 = torch.cat([x2, x2.new_ones(batch_size, num_pts,1)], dim=-1).reshape(batch_size, num_pts,3,1)
+    F = F.reshape(-1,1,3,3).repeat(1,num_pts,1,1)
+    x2Fx1 = torch.matmul(x2.transpose(2,3), torch.matmul(F, x1)).reshape(batch_size,num_pts)
+    Fx1 = torch.matmul(F,x1).reshape(batch_size,num_pts,3)
+    Ftx2 = torch.matmul(F.transpose(2,3),x2).reshape(batch_size,num_pts,3)
+    ys = x2Fx1**2 * (
+            1.0 / (Fx1[:, :, 0]**2 + Fx1[:, :, 1]**2 + 1e-15) +
+            1.0 / (Ftx2[:, :, 0]**2 + Ftx2[:, :, 1]**2 + 1e-15))
+    return ys
+
+def batch_symeig(X):
+    X = X.cpu()
+    b, d, _ = X.size()
+    bv = X.new(b, d, d)
+    for batch_idx in range(X.shape[0]):
+        e, v = torch.symeig(X[batch_idx, :, :].squeeze(), True)
+        bv[batch_idx, :, :] = v
+    bv = bv.cuda()
+    return bv
+
+def weighted_8points(x_in, weight):
+
+    x_shp = x_in.shape
+
+    weights = torch.relu(torch.tanh(weight))
+    x_in = x_in.squeeze(1)
+    xx = torch.reshape(x_in, (x_shp[0], x_shp[2], 4)).permute(0, 2, 1)
+
+
+    X = torch.stack([
+        xx[:, 2] * xx[:, 0], xx[:, 2] * xx[:, 1], xx[:, 2],
+        xx[:, 3] * xx[:, 0], xx[:, 3] * xx[:, 1], xx[:, 3],
+        xx[:, 0], xx[:, 1], torch.ones_like(xx[:, 0])
+    ], dim=1).permute(0, 2, 1)
+    wX = torch.reshape(weights, (x_shp[0], x_shp[2], 1)) * X
+    XwX = torch.matmul(X.permute(0, 2, 1), wX)
+    v = batch_symeig(XwX)
+    e_hat = torch.reshape(v[:, :, 0], (x_shp[0], 9))
+    e_hat = e_hat / torch.norm(e_hat, dim=1, keepdim=True)
+    return e_hat
